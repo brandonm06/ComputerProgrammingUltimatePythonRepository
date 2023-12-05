777d1c08be01b144c09911cb544ce05f5ff3f6fa
Brandon Morales <25brandonmorales@usd266.com>
2023-12-05 10:29:16 -0600

    finish with errors

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -95,3 +95,73 @@ def has_vowel(letters) :
 print("has_vowel")
 print("a,h,j,e,h,s", has_vowel(["a","h","j","e","h","s"]))
 print("l,m,n,p", has_vowel(["l","m","n","p"]))
+
+def all_the_same(numbers) :
+    first = numbers[0]
+    for num in numbers :
+        if num != first :
+            return False
+    return True
+print("All the same")
+print("5,5,5", all_the_same([5,5,5]))
+print("5,4,5", all_the_same([5,4,5]))
+
+def increasing(list) :
+    greaternum = list[0]-1
+    for number in list :
+        if number > greaternum :
+            greaternum = number
+        else: return False
+    return True
+
+print("increasing")
+print("1,2,4", increasing([1,2,4]))
+print("4,6,2", increasing([4,6,2]))
+print("0,1,4", increasing([0,1,4]))
+
+def is_incrementing(list) : 
+    nextnumber = list[0]+1
+    for number in list :
+        if number+1 == nextnumber :
+            nextnumber = nextnumber+1
+        else: return False
+    return True
+
+print("Is Incrementing")
+print("1,2,3", is_incrementing([1,2,3]))
+print("4,6,2", is_incrementing([4,6,2]))
+print("0,1,4", is_incrementing([0,1,4]))
+
+
+def has_adjacent_repeat(list) :
+    specialnumb = list[1]
+    for number in list :
+        if number == specialnumb :
+            return True
+        else: specialnumb = number
+    return False
+
+print("Has Adjacent Repeat")
+print("1,2,2", has_adjacent_repeat([1,2,2]))
+print("1,2,3", has_adjacent_repeat([1,2,3]))
+print("0,1,4", has_adjacent_repeat([0,1,4]))
+
+def sum_with_skips(list) :
+    badnumber = -1
+    ignoring = False
+    total = 0
+   
+    for number in list :
+        if number == badnumber:
+            ignoring = True
+        elif ignoring == True and number == badnumber:
+            ignoring = False
+        elif ignoring == False:
+            total = total + number
+
+    return total
+
+print("1,2,-1,2,1,-1,1", sum_with_skips([1,2,-1,2,1,-1,1]))
+print("1,1,-1,1,-1,1", sum_with_skips([1,1,-1,1,-1,1]))
+
+        
