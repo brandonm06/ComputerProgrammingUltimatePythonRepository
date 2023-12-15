a1e06145cec5124b7b3ae5473e70e85939327e51
Brandon Morales <25brandonmorales@usd266.com>
2023-12-15 10:03:10 -0600

    largestcitiesstart

--- 09WorkingWithFiles/Assignment/cities.py
+++ 09WorkingWithFiles/Assignment/cities.py
@@ -2,3 +2,30 @@ import json
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+f = open("../data/1000-largest-us-cities.json", "r")
+cities = json.load(f)
+f.close()
+
+
+
+def kansascities() : 
+    citylist = []
+    for city in cities:
+        if city["state"] == "Kansas" :
+            citylist.append(city["city"])
+    print(citylist)
+
+
+def longestname() :
+    longestcity = ""
+    longestcitychars = 0
+    for city in cities :
+        if len(city["city"]) > longestcitychars :
+            longestcity = city["city"]
+            longestcitychars = len(city["city"])
+    print(longestcity)
+longestname()
+
+def 
+
--- 09WorkingWithFiles/Assignment/gradebook.py
+++ 09WorkingWithFiles/Assignment/gradebook.py
@@ -9,9 +9,6 @@ reader = csv.reader(f)
 for row in reader:
     data.append(row)
 
-print(data)
-
-for row in data:
     
 
 def totalgradesawarded() :
@@ -20,3 +17,68 @@ def totalgradesawarded() :
     Ccount = 0
     Dcount = 0
     Fcount = 0
+    for row in data :
+        percent = int(row[2])
+        if percent > 89 :
+            Acount = Acount + 1
+        elif percent > 79 :
+            Bcount = Bcount + 1
+        elif percent > 69 :
+            Ccount = Ccount +1 
+        elif percent > 59 :
+            Dcount =Dcount +1
+        elif percent > 0 :
+            Fcount = Fcount +1
+    print("A:", Acount,"B:", Bcount,"C:", Ccount,"D:", Dcount,"F:", Fcount)
+
+
+def averagegrades(): 
+    gradetotal = 0
+    gradecount = 0
+    for row in data :
+        grade = int(row[1])
+        score = int(row[2])
+        if grade == 9 :
+            gradecount = gradecount + 1
+            gradetotal = gradetotal + score
+    print("Freshman Average:", gradetotal/gradecount)
+    gradetotal = 0
+    gradecount = 0
+    for row in data :
+        grade = int(row[1])
+        score = int(row[2])
+        if grade == 10 :
+            gradecount = gradecount + 1
+            gradetotal = gradetotal + score
+    print("Sophomore Average:", gradetotal/gradecount)
+    gradetotal = 0
+    gradecount = 0
+    for row in data :
+        grade = int(row[1])
+        score = int(row[2])
+        if grade == 11 :
+            gradecount = gradecount + 1
+            gradetotal = gradetotal + score
+    print("Junior Average:", gradetotal/gradecount)
+    gradetotal = 0
+    gradecount = 0
+    for row in data :
+        grade = int(row[1])
+        score = int(row[2])
+        if grade == 12 :
+            gradecount = gradecount + 1
+            gradetotal = gradetotal + score
+    print("Senior Average:", gradetotal/gradecount)
+
+
+def failingseniors() :
+    namelist = []
+    for row in data :
+        grade = int(row[1])
+        score = int(row[2])
+        name = (row[0])
+        if grade == 12 and score < 60 :
+            namelist.append(name)
+    print(namelist)
+
+failingseniors()
